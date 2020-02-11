0f718e106907e2b32baba2daf68c0e23c6abbde7
main <andrevallestero@gmail.com>
2020-02-11 12:54:27 -0500

    Initial commit

---
+++ rk3399-hantro-patcher.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+KERNEL_DIR=linux-pinebook-pro
+PATCH_DIR=patches
+
+echo Applying patches in $KERNEL_DIR
+for i in $PATCH_DIR/04*.patch; do
+	echo applying $(basename "$i")
+	git apply --directory=$KERNEL_DIR --whitespace=nowarn $i
+done
